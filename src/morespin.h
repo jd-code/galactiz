835ccc51166b8da567af2c35a885d7ee40d9eeb6
Jean-Daniel Pauget <jd@disjunkt.com>
2005-03-01 10:55:13 +0000

    this check-in is not clear regarding the exact status of this tree, sorry
    git-svn-id: svn://sub.nekodune.net/subdisj/bzoums/galactiz@500 32c91515-bade-0310-a695-fcfe42eb2883

---
+++ src/morespin.h
@@ -0,0 +1,34 @@
+
+//#ifndef   GRAPEFRUIT_H_INC
+//#warning cinetic.h should not be included as is. grapefruit.h should be included instead
+//#else  // GRAPEFRUIT_H_INC
+//#ifdef	    GRAPEFRUIT_H_GLOBINST
+//#define	    AGCINETIC_H_GLOBINST
+//#endif //   GRAPEFRUIT_H_GLOBINST
+//#endif // GRAPEFRUIT_H_INC
+
+#ifndef MORESPIN_H_HEADER
+#define MORESPIN_H_HEADER
+
+
+#include <matcalc/matcalc.h>
+
+#include <grapefruit/grapefruit.h>
+
+namespace grapefruit
+{
+    class Mv_Spin2 : public Mvmt
+    {
+	    GLfloat speed1k;	//!< the spin-speed in radian per millisecond
+	    Uint32 t_end;	//!< when the movement must end or 0 for non-ending
+	    Vector3 axis;	//!< the axis of the spin movement
+
+	public:
+	    Mv_Spin2 (TDObj &td, Vector3 &axe, GLfloat speed, Uint32 duration);
+	    virtual int step (void);
+    };
+
+    
+}   // namespace grapefruit
+
+#endif	// MORESPIN_H_HEADER
